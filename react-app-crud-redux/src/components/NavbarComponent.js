249c62f150b3d9dd2dae1d99d59b072efcac3901
hanafiku <hanafiku@users.noreply.github.com>
2021-04-26 02:03:59 +0700

    navbar dan jumboton

---
+++ react-app-crud-redux/src/components/NavbarComponent.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText,
+  Container,
+} from "reactstrap";
+
+const NavbarComponent = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar color="light" light expand="md">
+        <Container>
+          <NavbarBrand href="/">Usmanhanafi</NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/usmanhanafi/REACT-APP-CRUD-REDUX">
+                  About Us
+                </NavLink>
+              </NavItem>
+            </Nav>
+            <NavbarText>Admin</NavbarText>
+          </Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavbarComponent;
